420a346f4797a8b304a366d8807b2d75404838ac
sdatta-23 <103424755+sdatta-23@users.noreply.github.com>
2022-09-17 11:27:42 -0400

    Update ConsumerService.java

--- src/main/java/com/Api/Stock/service/ConsumerService.java
+++ src/main/java/com/Api/Stock/service/ConsumerService.java
@@ -5,11 +5,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.Api.Stock.dto.QuotationCustomResponseDTO;
-import com.Api.Stock.response.quoteResponse;
+import com.Api.Stock.response.QuoteResponse;
 import com.Api.Stock.dto.Custom_Global_Quote;
 
 @Service
-public class ConsumerService {
+public class ConsumerService 
+{
 	
 	@Value("${alphavantage.api.host}")
 	private String externalHost;
@@ -21,35 +22,60 @@ public class ConsumerService {
 	private String apiKey;
 	
 
-	public String getStockQuotationsAsString(String Symbol) {
+	public String getStockQuotationsAsString(String symbol) 
+	{
 		
-		String url = externalHost+"/query?function="+globalquoteFunction+"&Symbol="+Symbol+"&apiKey="+apiKey;
-		RestTemplate rest_template = new RestTemplate();
+		String url = externalHost+"/query?function="+globalquoteFunction+"&symbol="+symbol+"&apikey="+apiKey;
+		RestTemplate rest_template = new RestTemplate();  
 		String stock_quotes = rest_template.getForObject(url, String.class);
 		System.out.println(stock_quotes);
 		return stock_quotes;
 	}
 
-	public QuotationCustomResponseDTO getQuotationServiceResponse(String Symbol) {
-		String url = externalHost+"/query?function="+globalquoteFunction+"&Symbol="+Symbol+"&apiKey="+apiKey;
+	public QuotationCustomResponseDTO getQuotationServiceResponse(String symbol) {
+		String url = externalHost+"/query?function="+globalquoteFunction+"&symbol="+symbol+"&apikey="+apiKey;
 		RestTemplate rest_template = new RestTemplate();
-		QuotationCustomResponseDTO qcDTO = new QuotationCustomResponseDTO();
-		quoteResponse response = rest_template.getForObject(url, quoteResponse.class);
+		QuotationCustomResponseDTO customResponseDTO = new QuotationCustomResponseDTO();
+		QuoteResponse response = rest_template.getForObject(url, QuoteResponse.class);
 		
-		if(response!=null) {		
+		if(response!=null) 
+		{		
 			Custom_Global_Quote customGlobalQuote= new Custom_Global_Quote();
+
 			customGlobalQuote.setSymbol(response.getGlobalquote().getSymbol());
 			customGlobalQuote.setOpen(response.getGlobalquote().getOpen());
 			customGlobalQuote.setHigh(response.getGlobalquote().getHigh());
 			customGlobalQuote.setLow(response.getGlobalquote().getLow());
 			customGlobalQuote.setPrice(response.getGlobalquote().getPrice());
-
+			customGlobalQuote.setVolume(response.getGlobalquote().getVolume());
 			
-			qcDTO.setCustQuote(customGlobalQuote);
+			double previousClose= Double.parseDouble(response.getGlobalquote().getPreviousClose());
+			double todayOpen = Double.parseDouble(response.getGlobalquote().getOpen());
+			double currentPrice= Double.parseDouble(response.getGlobalquote().getPrice());
+			
+			if(todayOpen>previousClose) 
+			{
+				customGlobalQuote.setDayStartIndicator("GREEN");
+			}
+				else 
+				{
+					customGlobalQuote.setDayStartIndicator("RED");
+				}
+			
+			if(currentPrice>previousClose) 
+			{
+				customGlobalQuote.setCurrentIndicator("GREEN");
+			}
+				else 
+				{
+					customGlobalQuote.setCurrentIndicator("RED");
+				}
+			
+			customResponseDTO.setCustQuote(customGlobalQuote);
 		}
 		
-		System.out.println(qcDTO);
-		return qcDTO;
+		System.out.println(customResponseDTO);
+		return customResponseDTO;
 	}
 
 }
